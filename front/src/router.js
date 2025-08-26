5241edde2368d78787db334775a6de5da90eb365
伊藤慎悟 <ito.shingo@marble-corp.co.jp>
2025-08-26 16:02:47 +0900

    router.js/vue.config.jsの編集

--- front/src/router.js
+++ front/src/router.js
@@ -1,9 +1,26 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import SignIn from "./views/SignIn.vue";
-import SignOut from "./views/SignOut.vue";
-import Top from "./views/Top.vue";
+import LogIn from "./views/LogIn.vue";
+import LogOut from "./views/LogOut.vue";
+import Menu from "./views/Menu.vue";
+import ClientsList from "./views/Clients/ClientsList.vue";
+import ClientsCreate from "./views/Clients/ClientsCreate.vue";
+import ClientsEdit from "./views/Clients/ClientsEdit.vue";
+import ClientsDelete from "./views/Clients/ClientsDelete.vue";
+import OrdersList from "./views/Orders/OrdersList.vue";
+import OrdersCreate from "./views/Orders/OrdersCreate.vue";
+import OrdersEdit from "./views/Orders/OrdersEdit.vue";
+import OrdersDelete from "./views/Orders/OrdersDelete.vue";
+import ProductsList from "./views/Products/ProductsList.vue";
+import ProductsCreate from "./views/Products/ProductsCreate.vue";
+import ProductsEdit from "./views/Products/ProductsEdit.vue";
+import ProductsDelete from "./views/Products/ProductsDelete.vue";
+import UsersList from "./views/Users/UsersList.vue";
+import UsersCreate from "./views/Users/UsersCreate.vue";
+import UsersEdit from "./views/Users/UsersEdit.vue";
+import UsersDelete from "./views/Users/UsersDelete.vue";
+import Sorry from "./views/Sorry.vue";
 
 Vue.use(Router);
 
@@ -12,20 +29,105 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/pages/sign-in.html",
-      name: "signIn",
+      path: "/pages/log-in.html",
+      name: "logIn",
       props: true,
-      component: SignIn,
+      component: LogIn,
     },
     {
-      path: "/pages/sign-out.html",
+      path: "/pages/log-out.html",
       name: "signOut",
-      component: SignOut,
+      component: LogOut,
     },
     {
-      path: "/pages/top.html",
-      name: "top",
-      component: Top,
+      path: "/pages/menu.html",
+      name: "menu",
+      component: Menu,
+    },
+     {
+      path: "/pages/clients/list.html",
+      name: "clientsList",
+      component: ClientsList,
+    },
+    {
+      path: "/pages/clients/create.html",
+      name: "clientsCreate",
+      component: ClientsCreate,
+    },
+    {
+      path: "/pages/clients/edit.html",
+      name: "clientsEdit",
+      component: ClientsEdit,
+    },
+    {
+      path: "/pages/clients/delete.html",
+      name: "clientsDelete",
+      component: ClientsDelete,
+    },
+    {
+      path: "/pages/orders/list.html",
+      name: "ordersList",
+      component: OrdersList,
+    },
+    {
+      path: "/pages/orders/create.html",
+      name: "ordersCreate",
+      component: OrdersCreate,
+    },
+    {
+      path: "/pages/orders/edit.html",
+      name: "ordersEdit",
+      component: OrdersEdit,
+    },
+    {
+      path: "/pages/orders/delete.html",
+      name: "ordersDelete",
+      component: OrdersDelete,
+    },
+        {
+      path: "/pages/products/list.html",
+      name: "productsList",
+      component: ProductsList,
+    },
+    {
+      path: "/pages/products/create.html",
+      name: "productsCreate",
+      component: ProductsCreate,
+    },
+    {
+      path: "/pages/products/edit.html",
+      name: "productsEdit",
+      component: ProductsEdit,
+    },
+    {
+      path: "/pages/products/delete.html",
+      name: "productsDelete",
+      component: ProductsDelete,
+    },
+    {
+      path: "/pages/users/list.html",
+      name: "usersList",
+      component: UsersList,
+    },
+    {
+      path: "/pages/users/create.html",
+      name: "usersCreate",
+      component: UsersCreate,
+    },
+    {
+      path: "/pages/users/edit.html",
+      name: "usersEdit",
+      component: UsersEdit,
+    },
+    {
+      path: "/pages/users/delete.html",
+      name: "usersDelete",
+      component: UsresDelete,
+    },
+     {
+      path: "*",
+      name: "sorry",
+      component: Sorry,
     },
   ],
 });
---
+++ front/src/views/Sorry.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Sorry</h1>
+    <p v-show="msg">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: "Sorry, an error occurred......",
+    },
+  },
+};
+</script>
