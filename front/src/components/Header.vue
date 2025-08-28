bfba239a5c9caddba846bcb1c53a28a2f683924e
伊藤慎悟 <ito.shingo@marble-corp.co.jp>
2025-08-28 14:24:21 +0900

    Header.vue新規作成
    *Header.vue

---
+++ front/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
+      <a href="/public/pages/menu.html" style="text-decoration: none; color: white"><h1>販売管理システム</h1></a>
+
+      <!-- Navbar Search(なし) -->
+      <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0"></form>
+      <!-- Navbar -->
+      <ul class="navbar-nav ml-auto ml-md-0">
+        <div class="container text-center">
+          <div v-if="isLogIn" class="row">
+            <p class="text-white h5">ログイン中のユーザー名：</p>
+            <p v-html="userName" class="mr-3 text-white h5"></p>
+            <a class="btn-light btn-sm" href="/public/pages/log-out.html" role="button">ログアウト</a>
+          </div>
+        </div>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script>
+import * as UserUtil from "@/utils/UserUtil";
+export default {
+  data() {
+    return {
+      isLogIn: false,
+      userName: "",
+    };
+  },
+  async mounted() {
+    this.isLogIn = UserUtil.isLogIn();
+    const query = this.$route.query;
+    this.userName = query.userName ? query.userName : UserUtil.currentUserInfo().userName;
+  },
+};
+</script>
