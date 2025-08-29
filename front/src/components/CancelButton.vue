d8bc0c43a092c9e21228929d5e7da6a88d2418ec
横田凌也 <yokota.ryoya@marble-corp.co.jp>
2025-08-29 10:58:54 +0900

    取消ボタン
    *　componentsにCancelButton.vue作成

---
+++ front/src/components/CancelButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <button type="button" class="btn btn-secondary" v-on:click="onClickOrdersButton()">取消</button>
+</template>
+<script>
+export default {
+  methods: {
+    onClickOrdersButton: async function () {
+      window.location.reload(true);
+    },
+  },
+};
+</script>
