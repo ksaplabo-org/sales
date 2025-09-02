e2019d8226c223843173a22984fd539e0a32431a
横田凌也 <yokota.ryoya@marble-corp.co.jp>
2025-09-02 15:18:20 +0900

    取消ボタン完成
    *CancelButton.vue 取消ボタンを修正

--- front/src/components/CancelButton.vue
+++ front/src/components/CancelButton.vue
@@ -1,11 +1,16 @@
 <template>
-  <button type="button" class="btn btn-secondary" v-on:click="onClickOrdersButton()">取消</button>
+  <div class="col-2">
+    <button type="button" class="btn btn-secondary btn-lg btn-block" v-on:click="onClickCancelButton()">取消</button>
+  </div>
 </template>
 <script>
 export default {
   methods: {
-    onClickOrdersButton: async function () {
-      window.location.reload(true);
+    onClickCancelButton: async function () {
+      var cancelConfirm = window.confirm("入力内容を取り消しますか？");
+      if (cancelConfirm) {
+        window.location.reload(true);
+      }
     },
   },
 };
