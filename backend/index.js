bb127b7db66fcef07e4ce67bb7847372a886b7e8
横田凌也 <yokota.ryoya@marble-corp.co.jp>
2025-09-02 14:15:05 +0900

    顧客情報登録
    * 顧客情報登録処理と顧客情報登録画面の作成

--- backend/index.js
+++ backend/index.js
@@ -44,3 +44,27 @@ app.post("/api/sign-in", async function (req, res) {
   }
   res.status(status).send(resBody);
 });
+
+app.post("/api/clients", async function (req, res) {
+  // リクエストボディ取得
+  const reqBody = req.body;
+
+  try {
+    await ClientsLogic.create(
+      db,
+      reqBody.name,
+      reqBody.postCode,
+      reqBody.address1,
+      reqBody.address2,
+      reqBody.telNo,
+      reqBody.updateId,
+      reqBody.entryId
+    );
+    console.log("errtest");
+    res.send();
+  } catch (e) {
+    // 異常レスポンス
+    console.log("failed to add clients.", e);
+    res.status(500).send("server error occur");
+  }
+});
--- front/src/utils/AjaxUtil.js
+++ front/src/utils/AjaxUtil.js
@@ -9,3 +9,17 @@ export async function signIn(userId, password) {
     password: password,
   });
 }
+
+export async function postClients(clientsModel) {
+  const url = "/api/clients";
+
+  return await axios.post(url, {
+    name: clientsModel.name,
+    postCode: clientsModel.postCode,
+    address1: clientsModel.address1,
+    address2: clientsModel.address2,
+    telNo: clientsModel.telNo,
+    updateId: UserUtil.currentUserInfo().id,
+    entryId: UserUtil.currentUserInfo().id,
+  });
+}
