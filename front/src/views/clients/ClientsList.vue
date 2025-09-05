ed9b8c6479858e42c23f821d279a68ceda5caa91
佐藤駿介 <sato.shunsuke.1@marble-corp.co.jp>
2025-09-05 14:44:00 +0900

    変更

--- front/src/views/clients/ClientsList.vue
+++ front/src/views/clients/ClientsList.vue
@@ -10,72 +10,8 @@
 
         <!-- コンテンツStart -->
         <div style="width: 90%; margin: auto">
-<<<<<<< HEAD
           <!-- インポートしたテーブル -->
           <Table :items="items" :fields="fields" @sendRow="receiveRow" />
-=======
-          <!-- テーブル上部Start -->
-          <div class="row mb-3">
-            <!-- 表示件数切り替え -->
-            <div class="col-md-4">
-              <div class="form-inline">
-                <b-form-select id="per-page-select" v-model="perPage" :options="pageOptions"></b-form-select>
-                <label for="per-page-select">件表示</label>
-              </div>
-            </div>
-
-            <!-- 検索機能 -->
-            <div class="col-md-4 ml-auto">
-              <div class="form-inline">
-                <label for="filter-input">検索：</label>
-                <b-form-input id="filter-input" v-model="filter" type="search"></b-form-input>
-              </div>
-            </div>
-          </div>
-          <!-- テーブル上部End -->
-
-          <!-- テーブル本体 -->
-          <b-table
-            id="clientsTable"
-            :items="items"
-            :fields="fields"
-            :select-mode="'single'"
-            :per-page="perPage"
-            :current-page="currentPage"
-            :filter="filter"
-            striped
-            hover
-            selectable
-            show-empty
-            empty-text="顧客情報なし"
-            empty-filtered-text="検索結果がありません"
-            @row-selected="onRowSelected"
-          >
-          </b-table>
-
-          <!-- テーブル下部Start -->
-          <div class="row mb-3">
-            <!-- 件数表示 -->
-            <div class="col-md-4">
-              <p>計{{rows}}件</p>
-            </div>
-
-            <!-- ページング機能 -->
-            <div class="col-md-4 ml-auto">
-              <div class="form-inline">
-                <b-pagination
-                  v-model="currentPage"
-                  :total-rows="rows"
-                  :per-page="perPage"
-                  prev-text="前へ"
-                  next-text="次へ"
-                  aria-controls="clientsTable"
-                ></b-pagination>
-              </div>
-            </div>
-          </div>
-          <!-- テーブル下部End -->
->>>>>>> feature/2025/ordersList
 
           <!-- 登録・修正・削除ボタンStart -->
           <div class="form-group d-flex justify-content-center">
@@ -83,20 +19,12 @@
               <button class="btn btn-primary btn-block" v-on:click="onClickCreateButton()">登録</button>
             </div>
             <div class="p-2 w-25">
-<<<<<<< HEAD
               <button class="btn btn-info btn-block" v-on:click="onClickEditButton()" :disabled="clientRow == null">
-=======
-              <button class="btn btn-info btn-block" v-on:click="onClickEditButton()" :disabled="clientNo == null">
->>>>>>> feature/2025/ordersList
                 修正
               </button>
             </div>
             <div class="p-2 w-25">
-<<<<<<< HEAD
               <button class="btn btn-danger btn-block" v-on:click="onClickDeleteButton()" :disabled="clientRow == null">
-=======
-              <button class="btn btn-danger btn-block" v-on:click="onClickDeleteButton()" :disabled="clientNo == null">
->>>>>>> feature/2025/ordersList
                 削除
               </button>
             </div>
@@ -124,28 +52,17 @@ import Loading from "@/components/Loading.vue";
 import * as UserUtil from "@/utils/UserUtil";
 import * as AjaxUtil from "@/utils/AjaxUtil";
 import UserConst from "@/utils/const/UserConst";
-<<<<<<< HEAD
 import Table from "../../components/Table.vue";
 
 export default {
   props: ["flashMsg"],
   components: { Header, Loading, Table },
-=======
-
-export default {
-  props: ["flashMsg"],
-  components: { Header, Loading },
->>>>>>> feature/2025/ordersList
   data() {
     return {
       msg: this.flashMsg,
       errMsg: "",
       isLoading: false,
-<<<<<<< HEAD
       clientRow: null,
-=======
-      clientNo: null,
->>>>>>> feature/2025/ordersList
 
       //テーブル定義
       items: [],
@@ -157,24 +74,12 @@ export default {
         { key: "address2", label: "住所2", sortable: false },
         { key: "tel_no", label: "電話番号", sortable: false },
       ],
-<<<<<<< HEAD
-=======
-      // ページング定義
-      currentPage: 1,
-      perPage: 5,
-      rows: null,
-      pageOptions: [5, 10, 15],
-      filter: null,
->>>>>>> feature/2025/ordersList
     };
   },
   async mounted() {
     try {
-<<<<<<< HEAD
       this.isLoading = true;
 
-=======
->>>>>>> feature/2025/ordersList
       //ログインチェック
       if (!UserUtil.isLogIn()) {
         this.$router.push({ name: "logIn", params: { flashMsg: "ログインしてください" } });
@@ -186,17 +91,10 @@ export default {
 
       // 顧客情報取得
       await this.getClients();
-<<<<<<< HEAD
     } catch (e) {
       this.$router.push({ name: "logIn", params: { flashMsg: "ログインしてください" } });
     } finally {
       this.isLoading = false;
-=======
-      // テーブルに表示するページ数を設定
-      this.rows = this.items.length;
-    } catch (e) {
-      this.$router.push({ name: "logIn", params: { flashMsg: "ログインしてください" } });
->>>>>>> feature/2025/ordersList
     }
   },
   methods: {
@@ -204,53 +102,24 @@ export default {
      *顧客情報取得処理
      */
     getClients: async function () {
-<<<<<<< HEAD
-=======
-      this.isLoading = true;
-
->>>>>>> feature/2025/ordersList
       this.msg = "";
       this.errMsg = "";
 
       try {
         const response = await AjaxUtil.getClients();
-<<<<<<< HEAD
         this.items = JSON.parse(response.data.Items);
-=======
-
-        this.items = JSON.parse(response.data.Items);
-        console.log(this.items);
->>>>>>> feature/2025/ordersList
       } catch (e) {
         this.msg = "";
         this.errMsg = "顧客情報取得処理に失敗しました";
         console.log(e);
-<<<<<<< HEAD
-=======
-      } finally {
-        this.isLoading = false;
->>>>>>> feature/2025/ordersList
       }
     },
 
     /*
-<<<<<<< HEAD
      *一覧のデータ選択時、一時的な値を格納する処理
      */
     receiveRow(clientRow) {
       this.clientRow = clientRow;
-=======
-     *行選択時処理
-     */
-    onRowSelected: async function (selectedRow) {
-      //選択解除時(selectedRow配列に値が入っていない場合)はnullに設定
-      if (selectedRow.length == 0) {
-        this.clientNo = null;
-      } else {
-        //selectedRowがオブジェクト配列になっているため、indexを0として取得している
-        this.clientNo = selectedRow[0].client_no;
-      }
->>>>>>> feature/2025/ordersList
     },
 
     /*
@@ -271,22 +140,14 @@ export default {
      *修正画面遷移
      */
     onClickEditButton: async function () {
-<<<<<<< HEAD
       this.$router.push({ name: "clientsEdit", query: { clientNo: this.clientRow.client_no } });
-=======
-      this.$router.push({ name: "clientsEdit", query: { clientNo: this.clientNo } });
->>>>>>> feature/2025/ordersList
     },
 
     /*
      *削除画面遷移
      */
     onClickDeleteButton: async function () {
-<<<<<<< HEAD
       this.$router.push({ name: "clientsDelete", query: { clientNo: this.clientRow.client_no } });
-=======
-      this.$router.push({ name: "clientsDelete", query: { clientNo: this.clientNo } });
->>>>>>> feature/2025/ordersList
     },
   },
 };
