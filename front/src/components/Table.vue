194a6b277067abee1413c518842ab403d7962d78
佐藤駿介 <sato.shunsuke.1@marble-corp.co.jp>
2025-09-12 16:13:32 +0900

    テーブルレイアウト修正

--- front/src/components/Table.vue
+++ front/src/components/Table.vue
@@ -1,75 +1,66 @@
 <template>
-  <div>
-    <div id="content-wrapper" class="bg-light">
-      <div class="container-fluid">
-        <!-- コンテンツStart -->
-        <div style="width: 90%; margin: auto">
-          <!-- テーブル上部Start -->
-          <div class="row mb-3">
-            <!-- 表示件数切り替え -->
-            <div class="col-md-4">
-              <div class="form-inline">
-                <b-form-select id="per-page-select" v-model="perPage" :options="pageOptions"></b-form-select>
-                <label for="per-page-select">件表示</label>
-              </div>
-            </div>
+  <div id="content-wrapper" class="bg-light">
+    <div class="container-fluid">
+      <!-- コンテンツStart -->
 
-            <!-- 検索機能 -->
-            <div class="col-md-4 ml-auto">
-              <div class="form-inline justify-content-end">
-                <label for="filter-input">検索：</label>
-                <b-form-input id="filter-input" v-model="filter" type="search"></b-form-input>
-              </div>
-            </div>
-          </div>
-          <!-- テーブル上部End -->
+      <!-- テーブル上部Start -->
+      <div class="form-inline d-flex justify-content-between mb-2">
+        <!-- 表示件数切り替え -->
+        <div class="form-group">
+          <b-form-select id="per-page-select" v-model="perPage" :options="pageOptions"></b-form-select>
+          <label for="per-page-select">件表示</label>
+        </div>
 
-          <!-- テーブル本体 -->
-          <b-table
-            id="listTable"
-            :items="items"
-            :fields="fields"
-            :select-mode="'single'"
-            :per-page="perPage"
-            :current-page="currentPage"
-            :filter="filter"
-            striped
-            hover
-            selectable
-            show-empty
-            :empty-text="empDataMsg"
-            empty-filtered-text="検索結果がありません"
-            @row-selected="onRowSelected"
-          >
-          </b-table>
-
-          <!-- テーブル下部Start -->
-          <div class="row mb-3">
-            <!-- 件数表示 -->
-            <div class="col-md-4">
-              <p>計 {{ items.length }} 件</p>
-            </div>
-
-            <!-- ページング機能 -->
-            <div class="col-md-4 ml-auto">
-              <div class="form-inline justify-content-end">
-                <b-pagination
-                  v-model="currentPage"
-                  :total-rows="items.length"
-                  :per-page="perPage"
-                  prev-text="前へ"
-                  next-text="次へ"
-                  aria-controls="listTable"
-                ></b-pagination>
-              </div>
-            </div>
-          </div>
-          <!-- テーブル下部End -->
+        <!-- 検索機能 -->
+        <div class="form-group">
+          <label for="filter-input">検索：</label>
+          <b-form-input id="filter-input" v-model="filter" type="search"></b-form-input>
         </div>
       </div>
-      <!-- コンテンツEnd -->
+      <!-- テーブル上部End -->
+
+      <!-- テーブル本体 -->
+      <b-table
+        id="listTable"
+        :items="items"
+        :fields="fields"
+        :select-mode="'single'"
+        :per-page="perPage"
+        :current-page="currentPage"
+        :filter="filter"
+        striped
+        hover
+        selectable
+        show-empty
+        :empty-text="empDataMsg"
+        empty-filtered-text="検索結果がありません"
+        @row-selected="onRowSelected"
+      >
+      </b-table>
+
+      <!-- テーブル下部Start -->
+      <div class="d-flex justify-content-between">
+        <!-- 件数表示 -->
+        <div>
+          <p>計 {{ items.length }} 件</p>
+        </div>
+
+        <!-- ページング機能 -->
+        <div>
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="items.length"
+            :per-page="perPage"
+            prev-text="前へ"
+            next-text="次へ"
+            aria-controls="listTable"
+          ></b-pagination>
+        </div>
+      </div>
+      <!-- テーブル下部End -->
     </div>
   </div>
+  <!-- コンテンツEnd -->
 </template>
 
 <script>
