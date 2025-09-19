69226c2688361b96695d52d04af8dcbf7fe8c8fc
佐藤駿介 <sato.shunsuke.1@marble-corp.co.jp>
2025-09-19 14:12:20 +0900

    商品情報一覧画面完成

--- front/src/views/products/ProductsList.vue
+++ front/src/views/products/ProductsList.vue
@@ -5,19 +5,19 @@
       <div id="content-wrapper" class="bg-light min-vh-100">
         <div class="container-fluid">
           <!-- タイトルとメニュー遷移ボタン -->
-          <h1 class="border-bottom">受注情報一覧</h1>
+          <h1 class="border-bottom">商品情報一覧</h1>
           <button class="btn btn-dark mb-4" v-on:click="onClickMenuButton()">メニュー画面へ</button>
 
           <!-- コンテンツStart -->
           <div style="width: 90%; margin: auto">
-            <Table :items="items" :fields="fields" :empDataMsg="'受注情報がありません'" @sendRow="setReceiveRow" />
+            <Table :items="items" :fields="fields" :empDataMsg="'商品情報がありません'" @sendRow="setReceiveRow" />
             <!-- 登録・修正・削除ボタンStart -->
             <div class="form-group d-flex justify-content-center">
               <div class="p-2 w-25">
                 <button class="btn btn-primary btn-block" v-on:click="onClickCreateButton()">登録</button>
               </div>
               <div class="p-2 w-25">
-                <button class="btn btn-info btn-block" v-on:click="onClickEditButton()" :disabled="orderRow == null">
+                <button class="btn btn-info btn-block" v-on:click="onClickEditButton()" :disabled="productRow == null">
                   修正
                 </button>
               </div>
@@ -25,7 +25,7 @@
                 <button
                   class="btn btn-danger btn-block"
                   v-on:click="onClickDeleteButton()"
-                  :disabled="orderRow == null"
+                  :disabled="productRow == null"
                 >
                   削除
                 </button>
@@ -48,13 +48,14 @@
 </template>
 
 <script>
-// 共通
+// 共通コンポーネント関係
 import Header from "@/components/Header.vue";
 import Loading from "@/components/Loading.vue";
+import Table from "../../components/Table.vue";
+
+// util関係
 import * as UserUtil from "@/utils/UserUtil";
 import * as AjaxUtil from "@/utils/AjaxUtil";
-import UserConst from "@/utils/const/UserConst";
-import Table from "../../components/Table.vue";
 
 export default {
   props: ["flashMsg"],
@@ -64,7 +65,7 @@ export default {
       msg: this.flashMsg,
       errMsg: "",
       isLoading: false,
-      orderRow: null,
+      productRow: null,
 
       //テーブル定義
       items: [],
@@ -81,41 +82,28 @@ export default {
       if (!UserUtil.isLogIn()) {
         this.$router.push({ name: "logIn", params: { flashMsg: "ログインしてください" } });
       }
-
-      const response = await AjaxUtil.getProducts();
-      this.items = JSON.parse(response.data.Items);
+      // 商品情報取得処理を呼び出す
+      await this.getProducts();
     } catch (e) {
       this.$router.push({ name: "logIn", params: { flashMsg: "ログインしてください" } });
     }
   },
   methods: {
     /*
-     *受注情報取得処理
+     *商品情報取得処理
      */
-    async getOrders() {
+    async getProducts() {
       this.isLoading = true;
 
       this.msg = "";
       this.errMsg = "";
 
       try {
-        const response = await AjaxUtil.getOrders();
-        const tmpResponse = JSON.parse(response.data.Items);
-        // 配列に入っている値を一つずつ取り出し、新しい変数を追加していく処理
-        this.items = tmpResponse.map((order) => {
-          return {
-            order_no: order.order_no,
-            client_no: order.client_no,
-            // 0埋めされた表示用の顧客番号
-            display_client_no: String(order.client_no).padStart(8, "0"),
-            order_date: String(order.order_date).replace(/-/g, "/"),
-            ship_date: String(order.ship_date).replace(/-/g, "/"),
-            deliver_date: String(order.deliver_date).replace(/-/g, "/"),
-          };
-        });
+        const response = await AjaxUtil.getProducts();
+        this.items = JSON.parse(response.data.Items);
       } catch (e) {
         this.msg = "";
-        this.errMsg = "受注情報取得処理に失敗しました。";
+        this.errMsg = "商品情報取得処理に失敗しました。";
         console.log(e);
       } finally {
         this.isLoading = false;
@@ -125,8 +113,8 @@ export default {
     /*
      *一覧選択行の情報を保持する
      */
-    setReceiveRow(orderRow) {
-      this.orderRow = orderRow;
+    setReceiveRow(productRow) {
+      this.productRow = productRow;
     },
 
     /*
@@ -140,21 +128,21 @@ export default {
      *登録画面遷移
      */
     onClickCreateButton() {
-      this.$router.push({ name: "ordersCreate" });
+      this.$router.push({ name: "productsCreate" });
     },
 
     /*
      *修正画面遷移
      */
     onClickEditButton() {
-      this.$router.push({ name: "ordersEdit", query: { orderNo: this.orderRow.order_no } });
+      this.$router.push({ name: "productsEdit", query: { productCode: this.productRow.product_code } });
     },
 
     /*
      *削除画面遷移
      */
     onClickDeleteButton() {
-      this.$router.push({ name: "ordersDelete", query: { orderNo: this.orderRow.order_no } });
+      this.$router.push({ name: "productsDelete", query: { productCode: this.productRow.product_code } });
     },
   },
 };
