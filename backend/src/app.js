3808a80ffd46f51f03ff87cd57bf64e602c46dbf
Sugiya7254 <sugiya.ayumu@marble-corp.co.jp>
2026-07-21 14:24:31 +0900

    app.jsを全体に共有いたします。

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import clientRoutes from "./routes/clientRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -9,3 +12,6 @@ app.listen(process.env.PORT || 3000);
 
 app.use("/api/login", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/clients", clientRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/orders", orderRoutes);
