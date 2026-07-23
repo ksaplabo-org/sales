adbef7e4d38dabe633b2e721a749aff54c3c8a48
kkkazuto <kuwayama.kazuto@marble-corp.co.jp>
2026-07-23 11:00:17 +0900

    app.jsを追記しました
    *動作確認のためproduct以外のimportとuseをコメントアウトしました

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,9 @@ import express from "express";
 
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import clientRoutes from "./routes/clientRoutes.js";
+// import clientRoutes from "./routes/clientRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-import orderRoutes from "./routes/orderRoutes.js";
+// import orderRoutes from "./routes/orderRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -12,6 +12,6 @@ app.listen(process.env.PORT || 3000);
 
 app.use("/api/login", authRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/clients", clientRoutes);
+//app.use("/api/clients", clientRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/orders", orderRoutes);
+//app.use("/api/orders", orderRoutes);
