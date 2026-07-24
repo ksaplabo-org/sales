87275768d6813c1e0000d1c60efe40c80f302531
semijob <semizuka.juan@marble-corp.co.jp>
2026-07-24 16:34:21 +0900

    Controller作成
    *更新の作成
    *時間取得の消去

--- backend/src/controllers/clientController.js
+++ backend/src/controllers/clientController.js
@@ -3,7 +3,73 @@ import NotFoundError from "../errors/NotFoundError.js";
 import clientService from "../services/ClientService.js";
 
 class ClientController {
-   /**
+  /**
+   * 取引先情報一覧取得
+   *
+   * @param {*} req リクエスト情報
+   * @param {*} res レスポンス情報
+   */
+
+  async findAll(req, res) {
+    try {
+      // クエリパラメータから検索条件を作成
+      const condition = {
+        clientCode: req.query.clientCode,
+        clientName: req.query.clientName,
+        orderKbn: req.query.orderKbn,
+      };
+
+      // 取引先情報一覧検索
+      const clients = await clientService.findAll(condition);
+      res.json(clients);
+    } catch (e) {
+      console.error(e);
+      res.status(500).send();
+    }
+  }
+
+  /**
+   * 取引先情報詳細取得
+   *
+   * @param {*} req リクエスト情報
+   * @param {*} res レスポンス情報
+   */
+  async findByCode(req, res) {
+    try {
+      // パスパラメータチェック
+      // パスパラメータの取引先コードが設定されていない場合
+      if (!req.params.clientCode) {
+        console.error(err);
+        res.status(400).send();
+      }
+
+      // パスパラメータの取引先コードが8桁で設定されていない場合
+      else if (req.params.clientCode.length !== 8) {
+        console.error(e);
+        res.status(400).send();
+      }
+
+      // パスパラメータの取引先コードが半角英数で設定されていない場合
+      else if (!/^[A-Za-z0-9]+$/.test(req.params.clientCode)) {
+        console.error(err);
+        res.status(400).send();
+      }
+
+      const client = await clientService.findByCode(req.params.clientCode);
+      res.json(client);
+    } catch (err) {
+      console.error(err);
+
+      if (e instanceof NotFoundError) {
+        // 存在チェックエラー
+        res.status(NotFoundError.status).send();
+      } else {
+        res.status(500).send();
+      }
+    }
+  }
+
+  /**
    * 取引先情報登録
    *
    * @param {*} req リクエスト情報
@@ -11,9 +77,6 @@ class ClientController {
    */
   async create(req, res) {
     try {
-
-      const now = new Date().toISOString();
-
       const client = {
         clientCode: req.body.clientCode,
         clientName: req.body.clientName,
@@ -24,8 +87,6 @@ class ClientController {
         telNumber: req.body.telNumber,
         createdId: req.body.createdId,
         updatedId: req.body.createdId,
-        createdAt: now,
-        usedFlg: false,
       };
 
       // 共通バリデーション
@@ -65,7 +126,7 @@ class ClientController {
       }
     }
   }
-    /**
+  /**
    * 取引先情報更新
    *
    * @param {*} req リクエスト情報
@@ -75,24 +136,22 @@ class ClientController {
     try {
       // パスパラメータチェック
       // パスパラメータの取引先コードが設定されていない場合
-        if (!req.params.clientCode) {
+      if (!req.params.clientCode) {
         console.error(err);
         res.status(400).send();
-        }
+      }
 
       // パスパラメータの取引先コードが8桁で設定されていない場合
-        else if (req.params.clientCode.length !== 8) {
+      else if (req.params.clientCode.length !== 8) {
         console.error(e);
         res.status(400).send();
-        }
+      }
 
       // パスパラメータの取引先コードが半角英数で設定されていない場合
-        else if (!/^[A-Za-z0-9]+$/.test(req.params.clientCode)) {
+      else if (!/^[A-Za-z0-9]+$/.test(req.params.clientCode)) {
         console.error(err);
         res.status(400).send();
-        }
-
-      const now = new Date().toISOString();
+      }
 
       const clientCode = req.params.clientCode;
       const orderKbn = req.params.orderKbn;
@@ -105,7 +164,6 @@ class ClientController {
         address2: req.body.address2,
         telNumber: req.body.telNumber,
         updatedId: req.body.updatedId,
-        updatedAt: now,
       };
 
       // 共通バリデーション
