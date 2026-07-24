5702267cff8c83f80e305ba5e5140aa0cbc0e79e
semijob <semizuka.juan@marble-corp.co.jp>
2026-07-24 13:51:55 +0900

    Routes機能追加
    *削除機能追加

--- backend/src/routes/clientRoutes.js
+++ backend/src/routes/clientRoutes.js
@@ -4,5 +4,6 @@ import clientController from "../controllers/ClientController.js";
 const clientRoutes = express.Router();
 
 clientRoutes.get("/", clientController.findAll.bind(clientController));
+clientRoutes.delete("/:clientCode", clientController.delete.bind(clientController));
 
 export default clientRoutes;
