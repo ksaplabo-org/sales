e289775238e8728cca92316e03235f62958cacec
semijob <semizuka.juan@marble-corp.co.jp>
2026-07-24 14:51:48 +0900

    Repository作成
    *登録・更新機能作成

--- backend/src/repositories/clientRepository.js
+++ backend/src/repositories/clientRepository.js
@@ -3,38 +3,35 @@ import ClientModel from "../models/ClientModel.js";
 
 class ClientRepository {
   /**
-   * 取引先情報一覧取得
+   * 取引先情報詳細取得
    *
-   * @param {*} condition 検索条件
-   * @returns 取引先情報一覧
+   * @param {*} code 取引先コード
+   * @returns 取引先情報
    */
-  async findAll(condition) {
-    // 検索条件を作成
-    const where = {};
-    if (condition.clientCode) {
-      where.clientCode = { [Op.like]: "%" + condition.clientCode + "%" };
-    }
-    if (condition.clientName) {
-      where.clientName = { [Op.like]: "%" + condition.clientName + "%" };
-    }
-    if (condition.orderKbn) {
-      where.orderKbn = condition.orderKbn;
-    }
+  async findByCode(code) {
+    return await ClientModel.findByPk(code);
+  }
 
-    // 検索結果を返却
-    return await ClientModel.findAll({
-      attributes: [
-        ["client_code", "clientCode"],
-        "orderKbn",
-        ["client_name", "clientName"],
-        ["post_code", "postCode"],
-        ["address1", "address1"],
-        ["address2", "address2"],
-        [fn("CONCAT", col("address1"), col("address2")), "fullAddress"],
-        ["tel_number", "telNumber"],
-        [literal("EXISTS(SELECT 1 FROM orders o WHERE o.client_code = ClientModel.client_code)"), "usedFlg"],
-      ],
-      where: where,
+  /**
+   * 取引先情報登録
+   *
+   * @param {*} clientInfo 取引先情報
+   */
+  async create(clientInfo) {
+    await ClientModel.create(clientInfo);
+  }
+
+  /**
+   * 取引先情報更新
+   *
+   * @param {*} clientCode 取引先コード
+   * @param {*} clientInfo 取引先情報
+   */
+  async update(clientCode, clientInfo) {
+    await ClientModel.update(clientInfo, {
+      where: {
+        clientCode: clientCode,
+      },
     });
   }
 }
