7ba156acab420caa42b037845edb4bce5ee6911b
Sugiya7254 <sugiya.ayumu@marble-corp.co.jp>
2026-07-24 15:59:17 +0900

    商品登録routesを新規作成
    *新規作成のため初期コミット

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -4,5 +4,7 @@ import productController from "../controllers/ProductController.js";
 const productRoutes = express.Router();
 
 productRoutes.get("/", productController.findAll.bind(productController));
+productRoutes.get("/:productCode", productController.findByCode.bind(productController));
+productRoutes.post("/", productController.create.bind(productController));
 
 export default productRoutes;
