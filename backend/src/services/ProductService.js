f4a10cf922b4c9f7b9bc5ca3bf69d47998fee18e
Sugiya7254 <sugiya.ayumu@marble-corp.co.jp>
2026-07-24 15:53:41 +0900

    商品登録serviceを新規作成
    *新規作成のため初期コミット

--- backend/src/services/ProductService.js
+++ backend/src/services/ProductService.js
@@ -12,6 +12,46 @@ class ProductService {
   async findAll(condition) {
     return await productRepository.findAll(condition);
   }
+
+  /**
+   * 商品情報詳細取得
+   *
+   * @param {*} productCode 商品コード
+   * @returns 商品情報詳細
+   */
+  async findByCode(productCode) {
+    const product = await productRepository.findByCode(productCode);
+    if (!product) {
+      throw new NotFoundError();
+    }
+    return product;
+  }
+
+  /**
+   * 商品情報登録
+   *
+   * @param {*} productInfo 商品情報
+   */
+  async create(productInfo) {
+    // 一意性制約チェック
+    const product = await productRepository.findByCode(productInfo.productCode);
+    if (product) {
+      throw new UniqueConstraintError();
+    }
+
+    //発注先コード存在チェック
+    // const productCode = await clientRepository.findByCode(productInfo.orderClientCode);
+    // if (!productCode) {
+    //   throw new NotFoundError();
+    // }
+
+    //現在日時を取得
+    const now = new Date().toISOString();
+    productInfo.createdAt = now;
+    productInfo.updatedAt = now;
+
+    await productRepository.create(productInfo);
+  }
 }
 
 export default new ProductService();
