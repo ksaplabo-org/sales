02aaa9845a9d7106d0b48b2628088dd4765b2d8c
Sugiya7254 <sugiya.ayumu@marble-corp.co.jp>
2026-07-24 15:54:03 +0900

    商品登録repositoryを新規作成
    *新規作成のため初期コミット

--- backend/src/repositories/ProductRepository.js
+++ backend/src/repositories/ProductRepository.js
@@ -15,21 +15,19 @@ class ProductRepository {
       where.orderKbn = condition.orderKbn;
     }
     if (condition.productCode) {
-        where.productCode = { [Op.eq]: condition.productCode};
+      where.productCode = { [Op.eq]: condition.productCode };
     }
     if (condition.productName) {
-      where.productName = { [Op.like]: "%" + condition.productName + "%"};
+      where.productName = { [Op.like]: "%" + condition.productName + "%" };
     }
     if (condition.productPriceLow || condition.productPriceHigh) {
       if (condition.productPriceLow && !condition.productPriceHigh) {
-        where.productPrice = { [Op.gte]: condition.productPriceLow};
+        where.productPrice = { [Op.gte]: condition.productPriceLow };
+      } else if (!condition.productPriceLow && condition.productPriceHigh) {
+        where.productPrice = { [Op.lte]: condition.productPriceHigh };
       }
-      else if (!condition.productPriceLow && condition.productPriceHigh) {
-        where.productPrice = { [Op.lte]: condition.productPriceHigh};
-      }
-      where.productPrice = { [Op.gte]: condition.productPriceLow, [Op.lte]: condition.productPriceHigh}; 
+      where.productPrice = { [Op.gte]: condition.productPriceLow, [Op.lte]: condition.productPriceHigh };
     }
-
 
     // 検索結果を返却
     return await ProductModel.findAll({
@@ -39,10 +37,29 @@ class ProductRepository {
         "orderKbn",
         ["order_client_code", "orderClientCode"],
         ["product_price", "productPrice"],
-        [literal('EXISTS(SELECT 1 FROM orders o WHERE o.product_code = ProductModel.product_code)'),'usedFlg']
+        [literal("EXISTS(SELECT 1 FROM orders o WHERE o.product_code = ProductModel.product_code)"), "usedFlg"],
       ],
       where: where,
     });
+  }
+
+  /**
+   * 商品情報詳細取得
+   *
+   * @param {*} productCode 商品コード
+   * @returns 商品情報
+   */
+  async findByCode(productCode) {
+    return await ProductModel.findByPk(productCode);
+  }
+
+  /**
+   * 情報登録
+   *
+   * @param {*} productInfo 商品情報
+   */
+  async create(productInfo) {
+    await ProductModel.create(productInfo);
   }
 }
 
