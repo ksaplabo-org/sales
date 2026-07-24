fab659dbb058e5a495dd97822a7319a4ff927cb6
kkkazuto <kuwayama.kazuto@marble-corp.co.jp>
2026-07-24 13:40:12 +0900

    ProductRepositoryの修正
    *検索条件の条件分岐を修正

--- backend/src/repositories/ProductRepository.js
+++ backend/src/repositories/ProductRepository.js
@@ -15,21 +15,22 @@ class ProductRepository {
       where.orderKbn = condition.orderKbn;
     }
     if (condition.productCode) {
-        where.productCode = { [Op.eq]: condition.productCode};
+      where.productCode = { [Op.eq]: condition.productCode };
     }
     if (condition.productName) {
-      where.productName = { [Op.like]: "%" + condition.productName + "%"};
+      where.productName = { [Op.like]: "%" + condition.productName + "%" };
     }
     if (condition.productPriceLow || condition.productPriceHigh) {
-      if (condition.productPriceLow && !condition.productPriceHigh) {
-        where.productPrice = { [Op.gte]: condition.productPriceLow};
-      }
-      else if (!condition.productPriceLow && condition.productPriceHigh) {
-        where.productPrice = { [Op.lte]: condition.productPriceHigh};
-      }
-      where.productPrice = { [Op.gte]: condition.productPriceLow, [Op.lte]: condition.productPriceHigh}; 
-    }
+      where.productPrice = {};
 
+      if (condition.productPriceLow) {
+        where.productPrice[Op.gte] = condition.productPriceLow;
+      }
+
+      if (condition.productPriceHigh) {
+        where.productPrice[Op.lte] = condition.productPriceHigh;
+      }
+    }
 
     // 検索結果を返却
     return await ProductModel.findAll({
@@ -39,7 +40,7 @@ class ProductRepository {
         "orderKbn",
         ["order_client_code", "orderClientCode"],
         ["product_price", "productPrice"],
-        [literal('EXISTS(SELECT 1 FROM orders o WHERE o.product_code = ProductModel.product_code)'),'usedFlg']
+        [literal("EXISTS(SELECT 1 FROM orders o WHERE o.product_code = ProductModel.product_code)"), "usedFlg"],
       ],
       where: where,
     });
