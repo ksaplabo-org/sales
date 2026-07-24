04d46efb285511358d4819282c8e2199d7efb273
semijob <semizuka.juan@marble-corp.co.jp>
2026-07-24 14:51:21 +0900

    Service作成
    *登録・更新機能作成

--- backend/src/services/clientService.js
+++ backend/src/services/clientService.js
@@ -5,13 +5,47 @@ import clientRepository from "../repositories/ClientRepository.js";
 
 class ClientService {
   /**
-   * 取引先情報一覧取得
+   * 取引先情報詳細取得
    *
-   * @param {*} condition 検索条件
-   * @returns 取引先情報一覧
+   * @param {*} id 取引先コード
+   * @returns 取引先情報詳細
    */
-  async findAll(condition) {
-    return await clientRepository.findAll(condition);
+  async findByCode(code) {
+    const client = await clientRepository.findByCode(code);
+    if (!client) {
+      throw new NotFoundError();
+    }
+    return client;
+  }
+
+  /**
+   * 取引先情報登録
+   *
+   * @param {*} clientInfo 取引先情報
+   */
+  async create(clientInfo) {
+    // 一意性制約チェック
+    const client = await clientRepository.findByCode(clientInfo.clientCode);
+    if (client) {
+      throw new UniqueConstraintError();
+    }
+
+    await clientRepository.create(clientInfo);
+  }
+
+  /**
+   * 取引先情報更新
+   *
+   * @param {*} clientCode 取引先コード
+   * @param {*} clientInfo 取引先情報
+   */
+  async update(clientCode, clientInfo) {
+    // 更新データの存在チェック
+    const client = await clientRepository.findByCode(clientCode);
+    if (!client) {
+      throw new NotFoundError();
+    }
+    await clientRepository.update(clientCode, clientInfo);
   }
 }
 
