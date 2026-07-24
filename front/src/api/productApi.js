f8d14b7ac49f39019b3520f063c65580f208e43e
kkkazuto <kuwayama.kazuto@marble-corp.co.jp>
2026-07-24 10:50:51 +0900

    productApiの修正
    *コメントの記載を修正

--- front/src/api/productApi.js
+++ front/src/api/productApi.js
@@ -1,50 +1,50 @@
 import apiClient from "./axios";
 
 /**
- * ユーザー情報一覧取得API呼び出し
+ * 商品情報一覧取得API呼び出し
  *
  * @param {*} condition 検索条件
  * @returns ユーザー情報一覧
  */
-export async function getproducts(condition) {
+export async function getProducts(condition) {
   const response = await apiClient.get("/products", { params: condition });
   return response.data;
 }
 
 /**
- * ユーザー情報詳細取得API呼び出し
+ * 商品情報詳細取得API呼び出し
  *
- * @param {*} productId ユーザーID
- * @returns ユーザー情報
+ * @param {*} productCode ユーザーID
+ * @returns 商品情報
  */
-export async function getProductByProductId(productId) {
-  const response = await apiClient.get(`/users/${productId}`);
+export async function getProductByProductId(productCode) {
+  const response = await apiClient.get(`/users/${productCode}`);
   return response.data;
 }
 
 /**
- * ユーザー情報登録
+ * 商品情報登録
  *
- * @param {*} productInfo ユーザー情報
+ * @param {*} productInfo 商品情報
  */
 export async function createUser(productInfo) {
   await apiClient.post("/users/", productInfo);
 }
 
 /**
- * ユーザー情報更新
+ * 商品情報更新
  *
- * @param {*} productInfo ユーザー情報
+ * @param {*} productInfo 商品情報
  */
 export async function editProduct(productInfo) {
-  await apiClient.put(`/products/${productInfo.productId}`, productInfo);
+  await apiClient.put(`/products/${productInfo.productCode}`, productInfo);
 }
 
 /**
- * ユーザー情報削除
+ * 商品情報削除
  *
  * @param {*} productId ユーザーID
  */
-export async function deleteUser(productId) {
-  await apiClient.delete(`/products/${productId}`);
+export async function deleteUser(productCode) {
+  await apiClient.delete(`/products/${productCode}`);
 }
