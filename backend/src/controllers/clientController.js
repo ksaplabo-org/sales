683b0dfb047885f12dc3a4ce0af920c05778cf07
semijob <semizuka.juan@marble-corp.co.jp>
2026-07-24 13:50:04 +0900

    Controller機能追加
    *削除機能追加

--- backend/src/controllers/clientController.js
+++ backend/src/controllers/clientController.js
@@ -27,5 +27,44 @@ class ClientController {
       res.status(500).send();
     }
   }
+
+  /**
+   * 取引先情報削除
+   *
+   * @param {*} req リクエスト情報
+   * @param {*} res レスポンス情報
+   */
+  async delete(req, res) {
+    try {
+      // パスパラメータチェック
+      // パスパラメータの取引先コードが設定されていない場合
+      if (!req.params.clientCode) {
+        console.error(err);
+        res.status(400).send();
+      }
+
+      // パスパラメータの取引先コードが8桁で設定されていない場合
+      else if (req.params.clientCode.length !== 8) {
+        console.error(e);
+        res.status(400).send();
+      }
+
+      // パスパラメータの取引先コードが半角英数で設定されていない場合
+      else if (!/^[A-Za-z0-9]+$/.test(req.params.clientCode)) {
+        console.error(err);
+        res.status(400).send();
+      }
+      await clientService.delete(req.params.clientCode);
+      res.send();
+    } catch (e) {
+      console.log(e);
+
+      if (e instanceof NotFoundError) {
+        res.status(NotFoundError.status).send();
+      } else {
+        res.status(500).send();
+      }
+    }
+  }
 }
 export default new ClientController();
