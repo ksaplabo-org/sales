5494c046a184d99ce169677b96b517db9500ceb8
Fukushima97-f1 <fukushima.ryoga@marble-corp.co.jp>
2026-07-24 15:20:14 +0900

    clientApiの作成
    *取引先情報登録処理の追加

--- front/src/api/clientApi.js
+++ front/src/api/clientApi.js
@@ -10,3 +10,23 @@ export async function getClients(condition) {
   const response = await apiClient.get("/clients", { params: condition });
   return response.data;
 }
+
+/**
+ * 取引先情報詳細取得API呼び出し
+ *
+ * @param {*} clientCode 取引先コード
+ * @returns 取引先情報
+ */
+export async function getClientByCode(clientCode) {
+  const response = await apiClient.get(`/clients/${clientCode}`);
+  return response.data;
+}
+
+/**
+ * 取引先情報登録API呼び出し
+ *
+ * @param {*} clientInfo 取引先情報
+ */
+export async function createClient(clientInfo) {
+  await apiClient.post("/clients/", clientInfo);
+}
