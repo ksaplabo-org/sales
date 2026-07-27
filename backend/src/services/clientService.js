d70036d566e09f1eb4c890ee94d60fb0294b9cfa
semijob <semizuka.juan@marble-corp.co.jp>
2026-07-27 11:44:50 +0900

    Serviceの機能追加
    *詳細取得追加

--- backend/src/services/clientService.js
+++ backend/src/services/clientService.js
@@ -15,17 +15,31 @@ class ClientService {
   }
 
   /**
+   * 取引先情報詳細取得
+   *
+   * @param {*} id 取引先コード
+   * @returns 取引先情報詳細
+   */
+  async findByCode(code) {
+    const client = await clientRepository.findByCode(code);
+    if (!client) {
+      throw new NotFoundError();
+    }
+    return client;
+  }
+
+  /**
    * 取引先情報物理削除
    *
    * @param {*} clientCode 取引先コード
    */
   async delete(clientCode) {
-    // 削除データの存在チェック
-    // const client = await clientRepository.findByCode(clientCode);
-    // //const used = await orderRepository.findByAll(clientCode);
-    // if (!client) {
-    //   throw new NotFoundError();
-    // }
+    //削除データの存在チェック
+    const client = await clientRepository.findByCode(clientCode);
+    //const used = await orderRepository.findByAll(clientCode);
+    if (!client) {
+      throw new NotFoundError();
+    }
     // 削除データの外部参照チェック
     //if (used) {
     //throw new UniqueConstraintError();
