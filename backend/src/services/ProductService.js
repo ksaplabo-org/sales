db6de1337b6981ee2bda558fd1ebb5598186b7bd
Sugiya7254 <sugiya.ayumu@marble-corp.co.jp>
2026-07-27 15:04:36 +0900

    商品登録serviceを新規作成
    *updateを追加

--- backend/src/services/ProductService.js
+++ backend/src/services/ProductService.js
@@ -52,6 +52,65 @@ class ProductService {
 
     await productRepository.create(productInfo);
   }
+
+  /**
+   * 商品情報更新
+   *
+   * @param {*} productCode 商品コード
+   * @param {*} productInfo 商品情報
+   */
+  async update(productCode, productInfo) {
+    // 更新データの存在チェック
+    const product = await productRepository.findByCode(productCode);
+    if (!product) {
+      throw new NotFoundError();
+    }
+    
+    const orderKbn = product.orderKbn;
+    
+    const errors = [];
+    
+    //発注先コード
+    if (product.orderKbn == "2") {
+      if (!product.orderClientCode) {
+        errors.push({ field: "orderClientCode", message: "発注先コードが設定されていません" });
+      } else if (product.orderClientCode.length != 8) {
+        errors.push({ field: "orderClientCode", message: "発注先コードは8桁で設定してください" });
+      } else if (!/^[A-Za-z0-9]+$/.test(product.orderClientCode)) {
+        errors.push({ field: "orderClientCode", message: "発注先コードは半角英数で設定してください" });
+      }
+    } else if (product.orderKbn == "1") {
+      if (product.orderClientCode) {
+        errors.push({ field: "orderClientCode", message: "発注先コードは設定できません" });
+      }
+    }
+    
+    if (errors.length > 0 ) {
+      console.log(errors);
+      console.log(product.orderKbn);
+      const error = new Error();
+      
+      error.errors = errors;
+      error.status = 400;
+      
+      throw error;
+    }
+    
+    /*
+    //発注先コードの存在チェック
+    const orderClientCode = await clientRepository.findByCode(productInfo.orderClientCode);
+    
+    if (!orderClientCode) {
+      throw new NotFoundError();
+    }
+    */
+   
+    //現在日時を取得
+    const now = new Date().toISOString();;
+    productInfo.updatedAt = now;
+    
+    await productRepository.update(productCode, productInfo);
+  }
 }
 
 export default new ProductService();
