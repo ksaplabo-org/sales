a7569dadebf924622ad46106c89761a20482415b
Sugiya7254 <sugiya.ayumu@marble-corp.co.jp>
2026-08-04 14:41:45 +0900

    UniqueConstraintError.jsの修正
    *BaseErrorを継承いたしました。

--- backend/src/errors/UniqueConstraintError.js
+++ backend/src/errors/UniqueConstraintError.js
@@ -1,5 +1,11 @@
-class UniqueConstraintError extends Error {
+import BaseError from "./BaseError.js";
+
+class UniqueConstraintError extends BaseError {
   static status = 409;
+
+  constructor(field, message) {
+    super(field, message);
+  }
 }
 
 export default UniqueConstraintError;
