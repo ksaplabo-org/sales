9067e3aaa8f2a2090c54ae55b7da93914dde2693
oki0319 <fujimori.oki@marble-corp.co.jp>
2026-08-05 13:07:09 +0900

    clientRepositoryの修正
    * returnの追加

--- backend/src/repositories/clientRepository.js
+++ backend/src/repositories/clientRepository.js
@@ -55,7 +55,7 @@ class ClientRepository {
    *
    */
   async delete(clientCode) {
-    await ClientModel.destroy({
+    return await ClientModel.destroy({
       where: {
         client_code: clientCode,
       },
