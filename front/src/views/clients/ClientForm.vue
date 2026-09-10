861b918ee52e61e21734ace3f97b2b943c7cd799
semijob <semizuka.juan@marble-corp.co.jp>
2026-09-10 13:46:49 +0900

    バグ修正
    * 未入力時か正常入力時には、グレー枠で表示できるように修正

--- front/src/views/clients/ClientForm.vue
+++ front/src/views/clients/ClientForm.vue
@@ -73,7 +73,7 @@
           <BFormInput
             id="postCode"
             v-model="form.postCode"
-            :state="form.postCode.length === 0 || form.postCode.length === 7"
+            :state="form.postCode.length >= 1 && form.postCode.length <= 6 ? false : null"
             :formatter="formatPostCode"
             maxlength="7"
             placeholder="-は入力しないでください"
@@ -86,13 +86,13 @@
 
       <BRow class="mb-3">
         <BFormGroup label="住所1" label-for="address1" label-cols="3">
-          <BFormInput id="address1" v-model="form.address1" :state="form.address1.length >= 0" maxlength="20" />
+          <BFormInput id="address1" v-model="form.address1" maxlength="20" />
         </BFormGroup>
       </BRow>
 
       <BRow class="mb-3">
         <BFormGroup label="住所2" label-for="address2" label-cols="3">
-          <BFormInput id="address2" v-model="form.address2" :state="form.address2.length >= 0" maxlength="20" />
+          <BFormInput id="address2" v-model="form.address2" maxlength="20" />
         </BFormGroup>
       </BRow>
 
@@ -101,10 +101,7 @@
           <BFormInput
             id="telNumber"
             v-model="form.telNumber"
-            :state="
-              form.telNumber.length === 0 ||
-              (form.telNumber.length === 13 && /^\d{3}-\d{4}-\d{4}$/.test(form.telNumber))
-            "
+            :state="form.telNumber.length === 0? null : (form.telNumber.length === 13 && /^\d{3}-\d{4}-\d{4}$/.test(form.telNumber)) ? null : false"
             :formatter="formatTelNumber"
             maxlength="13"
             placeholder="例:xxx-xxxx-xxxx"
