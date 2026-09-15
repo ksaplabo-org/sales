ad50dc908d09e959805c53498d950dd5ac0220db
Sugiya7254 <sugiya.ayumu@marble-corp.co.jp>
2026-09-15 14:04:16 +0900

    バグの修正
    * バグ記録表No.5の内容の修正をいたしました。

--- front/src/views/products/ProductMaster.vue
+++ front/src/views/products/ProductMaster.vue
@@ -13,8 +13,8 @@
   </BContainer>
 
   <!-- トースト -->
-  <BToast class="w-100" v-model="showSuccessToastMs" variant="success" no-progress>{{ successToastText }}</BToast>
-  <BToast class="w-100" v-model="showFailedToastMs" variant="danger" no-progress>{{ failedToastText }}</BToast>
+  <BToast class="w-100" v-model="showSuccessToastMs" variant="success" no-progress no-close-button>{{ successToastText }}</BToast>
+  <BToast class="w-100" v-model="showFailedToastMs" variant="danger" no-progress no-close-button>{{ failedToastText }}</BToast>
 
   <!-- 検索条件 -->
   <BCard class="shadow-sm mb-3">
